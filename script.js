d3d11a38b03271b4fe7566d4f3c3afbcb7d6e575
saynqr <leonelleonsainz04@gmail.com>
2025-02-07 01:24:47 -0600

    añadir a pagina San-Valentin

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('yes-btn').addEventListener('click', function() {
-    document.getElementById('answer').textContent = "¡Yay! ¡Estoy muy feliz! 💖";
-    document.getElementById('answer').style.color = '#4caf50';
-});
-
-document.getElementById('no-btn').addEventListener('click', function() {
-    document.getElementById('answer').textContent = "¡Lo siento! 😢 Pero siempre seremos amigos. 💕";
-    document.getElementById('answer').style.color = '#f44336';
-});
