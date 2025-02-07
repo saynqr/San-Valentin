6874e0e4410c0fdfd18a52209278d1016b2972d1
Leonel <leonelleonsainz04@gmail.com>
2025-02-07 01:56:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("yes-btn").addEventListener("click", function() {
-    document.getElementById("answer").innerHTML = "¡Qué alegría! ❤️";
+    document.getElementById("answer").innerHTML = "¡Sabia que dirias que si! ❤️";
     document.getElementById("answer").style.color = "green";
 });
 
 document.getElementById("no-btn").addEventListener("click", function() {
-    document.getElementById("answer").innerHTML = "¡Lo intentaré otro año! 😅";
+    document.getElementById("answer").innerHTML = "¡aaaaaaaaaah, va, ahí se ve que no me amas! 😅";
     document.getElementById("answer").style.color = "red";
 });
